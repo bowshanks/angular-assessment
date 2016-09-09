babbe9f9ccb8dad3a7580e235503a217c388b8a8
Daniel Gardner <bowshanks@gmail.com>
2016-09-09 14:28:19 -0600

    Finished

---
+++ app/app.js
@@ -0,0 +1,30 @@
+angular.module('assessment',['ui.router'])
+  .config(function($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home',{
+        url: '/',
+        templateUrl: './views/home.html'
+      })
+      .state('shop',{
+        url: '/shop',
+        templateUrl: './views/shop.html'
+      })
+      .state('details',{
+        url: '/details/:id',
+        templateUrl: './views/product-details.html',
+        controller: 'detailsCtrl',
+        controllerAs: 'vm'
+      })
+      .state('blog',{
+        url: '/blog',
+        templateUrl: './views/blog.html'
+      })
+      .state('about',{
+        url: '/about',
+        templateUrl: './views/about.html'
+      })
+
+
+  });
